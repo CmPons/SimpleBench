c200025526b96c880d357c564d90a59409dcd31a
cmpons <cmpons@gmail.com>
2025-12-10 16:28:52 +0100

    fix: Remove sccache since it sometimes causes cargo to fail to compile

--- flake.nix
+++ flake.nix
@@ -5,15 +5,23 @@
     flake-utils.url = "github:numtide/flake-utils";
     semantic-release-cargo.url = "github:CmPons/nix-semantic-release-cargo";
   };
-  outputs = { nixpkgs, flake-utils, semantic-release-cargo, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      semantic-release-cargo,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
         crm = semantic-release-cargo.packages.${system}.default;
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
 
           packages = with pkgs; [
@@ -28,19 +36,14 @@
             pkg-config
             podman
             rust-analyzer
-            sccache
             zip
             wine64
             semantic-release
             crm
           ];
 
-          RUSTC_WRAPPER = "sccache";
           # For when podman and docker are installed
           CROSS_CONTAINER_ENGINE = "podman";
-
-          buildInputs = with pkgs; [ alsa-lib SDL2 freetype ];
-          nativeBuildInputs = with pkgs; [ alsa-lib SDL2 freetype ];
 
           shellHook = ''
             export PATH=$PATH:"$HOME/.cargo/bin/"
@@ -48,5 +51,6 @@
             exec ${pkgs.zsh}/bin/zsh
           '';
         };
-      });
+      }
+    );
 }
