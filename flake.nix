2f359e8e24388eedc07cf183d3643d543702cc30
cmpons <cmpons@gmail.com>
2025-12-14 12:29:40 +0100

    fix: Fix flake.nix so it uses a more standard approach and can be properly used as an input

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     semantic-release-cargo.url = "github:CmPons/nix-semantic-release-cargo";
-    crane.url = "github:ipetkov/crane";
+    cargo-simplebench-repo.url = "github:CmPons/SimpleBench";
   };
   outputs =
     {
       nixpkgs,
       flake-utils,
       semantic-release-cargo,
-      crane,
+      cargo-simplebench-repo,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -21,24 +21,43 @@
           inherit system;
           config.allowUnfree = true;
         };
+
         crm = semantic-release-cargo.packages.${system}.default;
 
-        craneLib = crane.mkLib pkgs;
+        rustPlatform = pkgs.rustPlatform;
 
-        # Common arguments for crane builds
-        commonArgs = {
-          src = craneLib.cleanCargoSource ./.;
-          strictDeps = true;
-        };
+        cargo-simplebench =
+          let
+            cargoToml = builtins.fromTOML (builtins.readFile "${cargo-simplebench-repo}/Cargo.toml");
+          in
+          rustPlatform.buildRustPackage {
+            pname = "cargo-simplebench";
+            version = "v${cargoToml.workspace.package.version}";
+            src = "${cargo-simplebench-repo}";
+            cargoLock = {
+              lockFile = "${cargo-simplebench-repo}/Cargo.lock";
+            };
 
-        # Build dependencies first (for caching)
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            # Some tests require access to the default network device to determine the MAC address
+            # This is hashed and then used to store the benchmark data per machine
+            doCheck = false;
 
-        # Build the cargo-simplebench binary
-        cargo-simplebench = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-          cargoExtraArgs = "-p cargo-simplebench";
-        });
+            # We are in a workspace specify just cargo-simplebench
+            cargoBuildFlags = [
+              "-p"
+              "cargo-simplebench"
+            ];
+            cargoTestFlags = [
+              "-p"
+              "cargo-simplebench"
+            ];
+
+            meta = with pkgs.lib; {
+              description = "A minimalist microbenchmarking framework for Rust with clear regression detection";
+              homepage = "https://github.com/CmPons/SimpleBench";
+              license = licenses.mit;
+            };
+          };
       in
       {
         packages = {
